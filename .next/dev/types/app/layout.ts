ec5416a0d5f6a1a769b1e5378ac574d40a7419ab
gpc886 <gpc886@users.noreply.github.com>
2026-01-21 20:50:22 +0800

    chore: 更新构建产物

--- .next/dev/types/app/layout.ts
+++ .next/dev/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: D:\EdgeLoad\--main (1)\--main\src\app\layout.tsx
+// File: /workspace/projects/src/app/layout.tsx
 import * as entry from '../../../../src/app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
--- .next/dev/types/app/page.ts
+++ .next/dev/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: D:\EdgeLoad\--main (1)\--main\src\app\page.tsx
+// File: /workspace/projects/src/app/page.tsx
 import * as entry from '../../../../src/app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
